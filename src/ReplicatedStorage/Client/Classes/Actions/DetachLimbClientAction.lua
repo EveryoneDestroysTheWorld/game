aefc45dff581376cc99d3cefb330aa909d86dda8
Christian Toney <christian@makuwro.com>
2024-06-07 13:50:21 -0400

    update for now

--- src/ReplicatedStorage/Client/Classes/Actions/DetachLimbClientAction.lua
+++ src/ReplicatedStorage/Client/Classes/Actions/DetachLimbClientAction.lua
@@ -4,8 +4,10 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local Players = game:GetService("Players");
 local ContextActionService = game:GetService("ContextActionService");
+local React = require(ReplicatedStorage.Shared.Packages.react);
 local ClientAction = require(script.Parent.Parent.ClientAction);
 type ClientAction = ClientAction.ClientAction;
+local RunService = game:GetService("RunService");
 
 local DetachLimbAction = {
   ID = 2;
@@ -13,15 +15,94 @@ local DetachLimbAction = {
   description = "Detach a limb of your choice. It only hurts a little bit.";
 };
 
+type LimbSelectionButtonProps = {
+  onActivate: () -> ();
+  shortcutCharacter: string;
+}
+
+local function LimbSelectionButton(props: LimbSelectionButtonProps)
+
+  local function onActivate()
+
+    props.onActivate();
+
+  end;
+
+  return React.createElement("TextButton", {
+    [React.Event.Activated] = onActivate;
+  }, {
+    UIListLayout = React.createElement("UIListLayout", {
+      SortOrder = Enum.SortOrder.LayoutOrder;
+    });
+    IconContainerFrame = React.createElement("Frame", {
+      BackgroundTransparency = 0.9;
+      BorderSizePixel = 0;
+    }, {
+      UIStroke = React.createElement("UIStroke", {});
+      IconImageLabel = React.createElement("ImageLabel", {});
+    });
+    ShortcutCharacterLabel = React.createElement("TextLabel", {
+      BackgroundTransparency = 1;
+      LayoutOrder = 2;
+      Text = props.shortcutCharacter;
+    });
+  });
+
+end;
+
+type LimbSelectionWindowProps = {
+  onLimbSelect = (limbName: string) -> ();
+};
+
+local function LimbSelectionWindow(props: LimbSelectionWindowProps)
+
+  local buttonComponents = {};
+  local limbInfo = {
+    {name = "Torso"; shortcutCharacter = "1";}
+    {name = "Head"; shortcutCharacter = "2";}
+  };
+  for _, limb in ipairs({}) do
+
+    table.insert(buttonComponents, React.createElement(LimbSelectionButton, {
+      onActivate = function()
+
+        props.onLimbSelect(limb.name);
+
+      end;
+    }));
+
+  end;
+
+  return React.createElement(React.StrictMode, {}, {
+    Container = React.createElement("Frame", {}, {
+      React.createElement("UIListLayout", {
+        Name = "UIListLayout";
+        Padding = UDim.new(0, 15);
+        SortOrder = Enum.SortOrder.LayoutOrder;
+      });
+      buttonComponents;
+    });
+  });
+
+end;
+
 function DetachLimbAction.new(): ClientAction
 
-  local limbSelectorGUI: ScreenGui = nil;
-  local selectedLimb: string? = nil;
+  -- Set up the UI.
   local player = Players.LocalPlayer;
+  local limbSelectorGUI: ScreenGui = Instance.new("ScreenGui");
+  limbSelectorGUI.Name = "LimbSelectorGUI";
+  limbSelectorGUI.Parent = player:WaitForChild("PlayerGui");
+  limbSelectorGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+  limbSelectorGUI.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets;
+  limbSelectorGUI.ResetOnSpawn = false;
+  limbSelectorGUI.DisplayOrder = 1;
+  limbSelectorGUI.Enabled = true;
 
   local function breakdown(self: ClientAction)
 
-    
+    limbFindingEvent:Disconnect();
+    limbSelectionEvent:Disconnect();
 
   end;
 
@@ -29,7 +110,7 @@ function DetachLimbAction.new(): ClientAction
 
     if selectedLimb then
 
-      ReplicatedStorage.Shared.Functions.ExecuteAction:InvokeServer(self.ID, "Detach Limb", selectedLimb);
+      ReplicatedStorage.Shared.Functions:FindFirstChild(`{player.UserId}_{DetachLimbAction.ID}`):InvokeServer();
 
     end;
 
@@ -42,16 +123,6 @@ function DetachLimbAction.new(): ClientAction
     activate = activate;
     breakdown = breakdown;
   });
-
-  -- Set up the limb selector UI.
-  limbSelectorGUI = Instance.new("ScreenGui");
-  limbSelectorGUI.Name = "LimbSelector";
-  limbSelectorGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
-  limbSelectorGUI.Parent = player.PlayerGui;
-  limbSelectorGUI.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets;
-  limbSelectorGUI.ResetOnSpawn = false;
-  limbSelectorGUI.DisplayOrder = 1;
-  limbSelectorGUI.Enabled = true;
 
   -- Listen for events.
   ContextActionService:BindAction("Select Limb", function() end, false, Enum.KeyCode.J, Enum.KeyCode.K);
