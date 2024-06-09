cfd64481d49dc2627ea6a6bd52b4991f0899a86a
Christian Toney <christian@makuwro.com>
2024-06-08 23:10:59 -0400

    remove debug

--- src/ServerStorage/Classes/Archetypes/ExplosiveMimicServerArchetype.lua
+++ src/ServerStorage/Classes/Archetypes/ExplosiveMimicServerArchetype.lua
@@ -262,7 +262,7 @@ function ExplosiveMimicServerArchetype.new(contestant: ServerContestant, round: 
 
       -- 3.) If the part is in front of the player, use Explosive Punch until the part is destroyed.
       local primaryPart = character.PrimaryPart;
-      if not primaryPart then print("wait"); continue; end;
+      if not primaryPart then continue; end;
 
       local frontResult = workspace:Raycast(primaryPart.CFrame.Position, primaryPart.CFrame.LookVector * 3, defaultRaycastParams);
       if frontResult and frontResult.Instance == targetPart then
