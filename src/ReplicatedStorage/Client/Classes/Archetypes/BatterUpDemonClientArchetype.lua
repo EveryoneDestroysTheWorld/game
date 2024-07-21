a09376d395959fc9a54476cf00ad520fc2fa0137
Christian Toney <christian@makuwro.com>
2024-07-20 22:24:42 -0400

    add metadata

---
+++ src/ReplicatedStorage/Client/Classes/Archetypes/BatterUpDemonClientArchetype.lua
@@ -0,0 +1,34 @@
+--!strict
+local ClientArchetype = require(script.Parent.Parent.ClientArchetype);
+local ClientContestant = require(script.Parent.Parent.ClientContestant);
+type ClientContestant = ClientContestant.ClientContestant;
+type ClientArchetype = ClientArchetype.ClientArchetype;
+
+local BatterUpDemonClientArchetype = {
+  ID = 2;
+  name = "Batter-Up Demon";
+  description = "You'll never strike out with this one.";
+  iconImage = "rbxassetid://18584519829";
+  actionIDs = {};
+  type = "Fighter" :: "Fighter";
+};
+
+function BatterUpDemonClientArchetype.new(contestant: ClientContestant): ClientArchetype
+
+  local function breakdown(self: ClientArchetype)
+
+  end;
+
+  return ClientArchetype.new({
+    ID = BatterUpDemonClientArchetype.ID;
+    iconImage = BatterUpDemonClientArchetype.iconImage;
+    name = BatterUpDemonClientArchetype.name;
+    description = BatterUpDemonClientArchetype.description;
+    actionIDs = BatterUpDemonClientArchetype.actionIDs;
+    type = BatterUpDemonClientArchetype.type;
+    breakdown = breakdown;
+  });
+
+end;
+
+return BatterUpDemonClientArchetype;
