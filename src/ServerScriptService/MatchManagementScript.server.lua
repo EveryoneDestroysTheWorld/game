ea9f627c83c6e41b02a6d2100060f6532c569518
Christian Toney <christian@makuwro.com>
2024-09-06 23:41:17 -0400

    Update writers

--- src/ServerScriptService/MatchManagementScript.server.lua
+++ src/ServerScriptService/MatchManagementScript.server.lua
@@ -1,7 +1,6 @@
 --!strict
 -- Profile.lua
--- Written by Christian "Sudobeast" Toney
--- Edits by Hati :))))
+-- Writers: Christian "Sudobeast" Toney and Hati :))))
 -- This script controls the round and lobby management stuff.
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
