ca3076b5c84cf503f64e636c373bf9b32e63566d
Christian Toney <christian@makuwro.com>
2024-09-07 09:46:23 -0400

    Add Super Hammer on client and server

---
+++ src/ServerStorage/Classes/Items/SuperHammerServerItem.lua
@@ -0,0 +1,45 @@
+--!strict
+-- This module represents a Super Hammer on the server side. 
+-- Programmer: Christian Toney (Christian_Toney)
+-- Designer: Christian Toney (Christian_Toney)
+-- © 2024 Beastslash
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local ServerContestant = require(script.Parent.Parent.ServerContestant);
+type ServerContestant = ServerContestant.ServerContestant;
+local ServerItem = require(script.Parent.Parent.ServerItem);
+type ServerItem = ServerItem.ServerItem;
+local SuperHammerClientItem = require(ReplicatedStorage.Client.Classes.Items.SuperHammerClientItem);
+local ServerRound = require(script.Parent.Parent.ServerRound);
+type ServerRound = ServerRound.ServerRound;
+
+local SuperHammerServerItem = {
+  ID = SuperHammerClientItem.ID;
+  name = SuperHammerClientItem.name;
+  description = SuperHammerClientItem.description;
+};
+
+function SuperHammerServerItem.new(contestant: ServerContestant, round: ServerRound): ServerItem
+
+  local function activate(self: ServerItem)
+    
+    
+  end;
+  
+  local function breakdown()
+    
+  end;
+
+  local item = ServerItem.new({
+    ID = SuperHammerServerItem.ID;
+    name = SuperHammerServerItem.name;
+    description = SuperHammerServerItem.description;
+    activate = activate;
+    breakdown = breakdown;
+  });
+  
+  return item;
+
+end;
+
+return SuperHammerServerItem;
