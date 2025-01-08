d283cbd77e15f7397719c5bb664f29b5301af34b
Christian Toney <christian@makuwro.com>
2025-01-08 00:41:55 -0500

    update

--- src/StarterPlayer/StarterPlayerScripts/MatchHUDScript/ReactComponents/ArchetypeSelector.lua
+++ src/StarterPlayer/StarterPlayerScripts/MatchHUDScript/ReactComponents/ArchetypeSelector.lua
@@ -3,14 +3,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local React = require(ReplicatedStorage.Shared.Packages.react);
 local Button = require(ReplicatedStorage.Client.ReactComponents.Button);
 local ClientArchetype = require(ReplicatedStorage.Client.Classes.ClientArchetype);
+type ClientArchetype = ClientArchetype.ClientArchetype;
 
 export type ArchetypeSelectorProperties = {
-
+  onArchetypeSelected: (archetype: ClientArchetype) -> ();
 }
 
 local function ArchetypeSelector(properties: ArchetypeSelectorProperties)
 
-  local archetypes, setArchetypes = React.useState({});
+  local archetypes, setArchetypes = React.useState({} :: {ClientArchetype});
   React.useEffect(function()
   
     task.spawn(function()
@@ -29,15 +30,28 @@ local function ArchetypeSelector(properties: ArchetypeSelectorProperties)
 
   end, {});
 
+  local archetypeComponents = {};
+  for _, archetype in archetypes do
+
+    table.insert(archetypeComponents, React.createElement(Button, {
+      Image = archetype.iconImage;
+      key = archetype.id;
+      [React.Event.Activated] = function()
+
+        properties.onArchetypeSelected(archetype);
+
+      end;
+    }));
+
+  end;
+
   return React.createElement("Frame", {
     LayoutOrder = 2;
     AutomaticSize = Enum.AutomaticSize.XY;
     BackgroundTransparency = 1;
   }, {
-    ArchetypeCategorySelector = React.createElement(Button, {
-      LayoutOrder = 1;
-      Text = "FAVORITES";
-    });
+    -- TODO: Implement category selector.
+    ArchetypeOptionList = React.createElement(React.Fragment, {}, archetypeComponents);
   });
 
 end;
