1025d3e732a605ba61a8f502347cc1e109569a5b
Christian Toney <christian.toney@beastslash.com>
2025-01-28 15:09:04 -0500

    ChangeBallTypeClientAction

--- src/ReplicatedStorage/Client/Classes/Actions/ChangeBallTypeClientAction.lua
+++ src/ReplicatedStorage/Client/Classes/Actions/ChangeBallTypeClientAction.lua
@@ -8,85 +8,80 @@ local Players = game:GetService("Players");
 
 local ReactRoblox = require(ReplicatedStorage.Shared.Packages["react-roblox"]);
 local React = require(ReplicatedStorage.Shared.Packages.react);
-local ClientAction = require(script.Parent.Parent.ClientAction);
 local HUDButton = require(script.Parent.Parent.Parent.ReactComponents.HUDButton);
-type ClientAction = ClientAction.ClientAction;
 local QuickSelectionMenu = require(ReplicatedStorage.Client.ReactComponents.QuickSelectionMenu);
+local types = require(ReplicatedStorage.Client.Modules.types);
 
 local ChangeBallTypeClientAction = {
   id = script.Name:sub(1, script.Name:gsub("ClientAction", ""):len());
   name = "Change Ball Type";
   description = "Do a change-up";
   iconImage = "rbxassetid://18464513809";
+  __index = {} :: types.ChangeBallTypeClientAction;
 };
 
-function ChangeBallTypeClientAction.new(): ClientAction
+local player = Players.LocalPlayer;
 
-  local player = Players.LocalPlayer;
-  local remoteName: string = `{player.UserId}_{ChangeBallTypeClientAction.id}`;
-  local _gui: ScreenGui? = nil;
-  local root;
+function ChangeBallTypeClientAction.new(): types.ChangeBallTypeClientAction
 
-  local function breakdown(self: ClientAction)
-    
-		ReplicatedStorage.Client.Functions.DestroyHUDButton:Invoke("Action", self.id);
-
-  end;
-
-  local function activate(self: ClientAction)
-
-    local gui = _gui or Instance.new("ScreenGui");
-    gui.ScreenInsets = Enum.ScreenInsets.None;
-    gui.Parent = player.PlayerGui;
-
-    root = ReactRoblox.createRoot(gui);
-    root:render(React.createElement(QuickSelectionMenu, {
-      options = {
-        {
-          key = "Regular";
-          labelText = "Regular Ball";
-          iconImage = "rbxassetid://139648735745838"
-        };
-      };
-      onSelectionConfirmed = function(selection)
-
-        root:unmount();
-        gui:Destroy();
-        _gui = nil;
-        ReplicatedStorage.Shared.Functions.ActionFunctions:FindFirstChild(remoteName):InvokeServer(selection.key);
-
-      end;
-    }));
-
-  end;
-
-  local function initialize(self: ClientAction)
-
-    ReplicatedStorage.Client.Functions.AddHUDButton:Invoke("Action", React.createElement(HUDButton, {
-      type = "Action";
-      key = self.id;
-      onActivate = function() 
-      
-        self:activate();
-
-      end;
-      iconImage = "rbxassetid://75206024784140";
-    }));
-
-  end;
-
-  local action = ClientAction.new({
+  local overwrittenProperties = {
     id = ChangeBallTypeClientAction.id;
     name = ChangeBallTypeClientAction.name;
     iconImage = ChangeBallTypeClientAction.iconImage;
     description = ChangeBallTypeClientAction.description;
-    activate = activate;
-    breakdown = breakdown;
-    initialize = initialize;
-  });
+    remoteFunction = ReplicatedStorage.Shared.Functions.ActionFunctions:FindFirstChild(`{player.UserId}_{ChangeBallTypeClientAction.id}`);
+  };
+
+  local action = (setmetatable(overwrittenProperties, ChangeBallTypeClientAction) :: any) :: types.ChangeBallTypeClientAction;
+
+  ReplicatedStorage.Client.Functions.AddHUDButton:Invoke("Action", React.createElement(HUDButton, {
+    type = "Action";
+    key = action.id;
+    onActivate = function() 
+    
+      action:activate();
+
+    end;
+    iconImage = "rbxassetid://75206024784140";
+  }));
 
   return action;
 
 end
+
+function ChangeBallTypeClientAction.__index:activate()
+
+  local gui = self.gui or Instance.new("ScreenGui");
+  self.gui = gui;
+  gui.ScreenInsets = Enum.ScreenInsets.None;
+  gui.Parent = player.PlayerGui;
+
+  local reactRoot = self.reactRoot or ReactRoblox.createRoot(gui);
+  self.reactRoot = reactRoot;
+  reactRoot:render(React.createElement(QuickSelectionMenu, {
+    options = {
+      {
+        key = "Regular";
+        labelText = "Regular Ball";
+        iconImage = "rbxassetid://139648735745838"
+      };
+    };
+    onSelectionConfirmed = function(selection)
+
+      reactRoot:unmount();
+      gui:Destroy();
+      self.gui = nil;
+      self.remoteFunction:InvokeServer(selection.key);
+
+    end;
+  }));
+
+end
+
+function ChangeBallTypeClientAction.__index:breakdown()
+    
+  ReplicatedStorage.Client.Functions.DestroyHUDButton:Invoke("Action", self.id);
+
+end;
 
 return ChangeBallTypeClientAction;
--- src/ReplicatedStorage/Client/Modules/types.lua
+++ src/ReplicatedStorage/Client/Modules/types.lua
@@ -5,6 +5,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local TurfWarContestantStatistics = require(ReplicatedStorage.Shared.TurfWarContestantStatistics);
 export type TurfWarContestantStatistics = TurfWarContestantStatistics.TurfWarContestantStatistics;
 
+export type ChangeBallTypeClientAction = ClientAction<{
+  gui: ScreenGui?;
+  reactRoot: any; -- React doesn't export RootType. :\
+  remoteFunction: RemoteFunction;
+  activate: (self: ChangeBallTypeClientAction) -> ();
+  breakdown: (self: ChangeBallTypeClientAction) -> ();
+}>;
+
 export type ClientAction<Extension = unknown> = ClientActionProperties & Extension & ClientActionMethods;
 
 export type ClientActionClass<ConstructorProperties = any, Action = any> = {
@@ -15,15 +23,15 @@ export type ClientActionMethods = {
 
   -- The function to activate the item on the server side.
   -- You can manually activate the item some other way too.
-  activate: (self: ClientActionProperties, ...any) -> ();
+  activate: (self: any, ...any) -> ();
 
   -- The function to "break down" the item. This usually runs after the round ends and sometimes after item use.
   -- You can manually break down the item some other way too.
-  breakdown: (self: ClientActionProperties) -> ();
+  breakdown: (self: any) -> ();
 
   -- The function to initialize the item. This usually runs after the player receives an item. 
   -- This function does not mean the player activated the item. Use :activate() instead.
-  initialize: (self: ClientActionProperties) -> ();
+  initialize: (self: any) -> ();
 
 }
 
