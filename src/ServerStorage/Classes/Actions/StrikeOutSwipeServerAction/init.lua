b57d8aa1795cecbb4e9d45c076c826dc4c9cbe42
Christian Toney <christian.toney@beastslash.com>
2025-03-02 11:52:35 -0500

    charge swing and play swing animation

--- src/ServerStorage/Classes/Actions/StrikeOutSwipeServerAction/init.lua
+++ src/ServerStorage/Classes/Actions/StrikeOutSwipeServerAction/init.lua
@@ -11,7 +11,6 @@ local IStrikeOutSwipeServerAction = require(ServerStorage.Interfaces.IStrikeOutS
 local IServerContestant = require(ServerStorage.Interfaces.IServerContestant);
 local IServerRound = require(ServerStorage.Interfaces.IServerRound);
 
-local chargeSwing = require(script.chargeSwing);
 local swingBat = require(script.swingBat);
 local createInventoryRemoteFunction = require(ServerStorage.Modules.createInventoryRemoteFunction);
 local createInventoryRemoteEvent = require(ServerStorage.Modules.createInventoryRemoteEvent);
@@ -34,15 +33,92 @@ function StrikeOutSwipeServerAction.new(contestant: IServerContestant, round: IS
   local maxBonusDamage = 10;
   local requiredStamina = 5;
   local touchedTimeLimitSeconds = 0.8;
+  local swingAnimationTrack;
 
   local function activate(self: IStrikeOutSwipeServerAction, shouldCharge: boolean)
 
     assert(contestant.attributes.archetypeMode == "Batter", "Contestant must be in batter mode to use this action.");
     assert(contestant.currentStamina >= requiredStamina, "Insufficient stamina.");
 
+    local function playSwingAnimation(): ()
+
+      local player = contestant.player;
+      if player and self.remoteFunction then
+    
+        self.remoteFunction:InvokeClient(player, shouldCharge);
+    
+      else
+        
+        local character = contestant.character;
+        local humanoid = if character then character:FindFirstChild("Humanoid") else nil;
+        local animator = if humanoid then humanoid:FindFirstChild("Animator") else nil;
+    
+        if animator and animator:IsA("Animator") then
+    
+          if swingAnimationTrack then
+    
+            swingAnimationTrack:Stop(0);
+    
+          end;
+    
+          local swingAnimation = Instance.new("Animation");
+          swingAnimation.AnimationId = `rbxassetid://123556732066116`;
+          local currentAnimationTrack = animator:LoadAnimation(swingAnimation);
+          currentAnimationTrack.Looped = false;
+          currentAnimationTrack:Play(if shouldCharge then 1 else 0, 1, if shouldCharge then 0 else 8);
+          swingAnimationTrack = currentAnimationTrack;
+    
+        end;
+    
+      end;
+    
+    end;
+    
     if shouldCharge then
 
-      chargeSwing(self);
+      --[[
+        Charges the contestant's bat before they swing it. The longer the charge, the bigger the WHAM!
+      ]]
+      local function chargeSwing(): ()
+
+        local originalChargeTime = DateTime.now().UnixTimestampMillis;
+        startChargeTimeMilliseconds = originalChargeTime;
+
+        playSwingAnimation();
+
+        task.spawn(function()
+        
+          while task.wait(0.05) and startChargeTimeMilliseconds == originalChargeTime do
+
+            local player = contestant.player;
+            if contestant.currentStamina <= requiredStamina then
+
+              -- Let the player know to stop charging.
+              if self.remoteEvent and player then
+
+                self.remoteEvent:FireClient(player);
+
+              end;
+
+              self:activate(false);
+              break;
+
+            else
+
+              contestant:setCurrentStamina(contestant.currentStamina - 1, {
+                actionID = self.id;
+                contestantID = contestant.id;
+              });
+
+            end;
+
+          end;
+
+        end);
+
+      end;
+
+      chargeSwing()
 
     else
 
