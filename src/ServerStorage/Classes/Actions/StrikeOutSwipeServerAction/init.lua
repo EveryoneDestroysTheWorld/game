fbffd513433b83852be8451899b337765e3779f6
Christian Toney <christian.toney@beastslash.com>
2025-03-02 11:48:56 -0500

    clean up init

--- src/ServerStorage/Classes/Actions/StrikeOutSwipeServerAction/init.lua
+++ src/ServerStorage/Classes/Actions/StrikeOutSwipeServerAction/init.lua
@@ -7,36 +7,88 @@ local ServerStorage = game:GetService("ServerStorage");
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 
 local StrikeOutSwipeClientAction = require(ReplicatedStorage.Client.Classes.Actions.StrikeOutSwipeClientAction);
-local types = require(ServerStorage.Modules.types);
+local IStrikeOutSwipeServerAction = require(ServerStorage.Interfaces.IStrikeOutSwipeServerAction);
+local IServerContestant = require(ServerStorage.Interfaces.IServerContestant);
+local IServerRound = require(ServerStorage.Interfaces.IServerRound);
 
 local chargeSwing = require(script.chargeSwing);
 local swingBat = require(script.swingBat);
 local createInventoryRemoteFunction = require(ServerStorage.Modules.createInventoryRemoteFunction);
 local createInventoryRemoteEvent = require(ServerStorage.Modules.createInventoryRemoteEvent);
 
+type IStrikeOutSwipeServerAction = IStrikeOutSwipeServerAction.IStrikeOutSwipeServerAction;
+type IServerContestant = IServerContestant.IServerContestant;
+type IServerRound = IServerRound.IServerRound;
+
 local StrikeOutSwipeServerAction = {
   id = StrikeOutSwipeClientAction.id;
   name = StrikeOutSwipeClientAction.name;
   description = StrikeOutSwipeClientAction.description;
-  __index = {
-    name = StrikeOutSwipeClientAction.name;
-    id = StrikeOutSwipeClientAction.id;
-    description = StrikeOutSwipeClientAction.description;
-  } :: types.StrikeOutSwipeServerAction;
 };
 
-function StrikeOutSwipeServerAction.new(properties: types.ServerActionConstructorProperties): types.StrikeOutSwipeServerAction
+function StrikeOutSwipeServerAction.new(contestant: IServerContestant, round: IServerRound): IStrikeOutSwipeServerAction
 
-  local action = (setmetatable({}, StrikeOutSwipeServerAction) :: any) :: types.StrikeOutSwipeServerAction;
-  action.contestant = properties.contestant;
-  action.startChargeTimeMilliseconds = 0;
-  action.maxChargeDurationMilliseconds = 1000;
-  action.baseDamage = 12;
-  action.maxBonusDamage = 10;
-  action.requiredStamina = 5;
-  action.touchedTimeLimitSeconds = 0.8;
+  local startChargeTimeMilliseconds = 0;
+  local maxChargeDurationMilliseconds = 1000;
+  local baseDamage = 12;
+  local maxBonusDamage = 10;
+  local requiredStamina = 5;
+  local touchedTimeLimitSeconds = 0.8;
 
-  local player = action.contestant.player;
+  local function activate(self: IStrikeOutSwipeServerAction, shouldCharge: boolean)
+
+    assert(contestant.attributes.archetypeMode == "Batter", "Contestant must be in batter mode to use this action.");
+    assert(contestant.currentStamina >= requiredStamina, "Insufficient stamina.");
+
+    if shouldCharge then
+
+      chargeSwing(self);
+
+    else
+
+      local character = contestant.character;
+      local bat = if character then character:FindFirstChild("Bat") else nil;
+      assert(bat and bat:IsA("Accessory"), "The contestant's bat is missing.");
+
+      swingBat(self, bat);
+
+    end;
+
+  end;
+
+  local function breakdown(self: IStrikeOutSwipeServerAction)
+
+    if self.remoteFunction then
+
+      self.remoteFunction:Destroy();
+  
+    end;
+  
+    if self.remoteEvent then
+  
+      self.remoteEvent:Destroy();
+  
+    end;
+  
+    if contestant.player then
+  
+      ReplicatedStorage.Shared.Functions.BreakdownAction:InvokeClient(contestant.player, self.id);
+  
+    end;
+
+  end;
+
+  local action: IStrikeOutSwipeServerAction = {
+    attributes = {};
+    contestantID = contestant.id;
+    description = StrikeOutSwipeServerAction.description;
+    id = StrikeOutSwipeServerAction.id;
+    name = StrikeOutSwipeServerAction.name;
+    activate = activate;
+    breakdown = breakdown;
+  }
+
+  local player = contestant.player;
   if player then
   
     local remoteID = `{player.UserId}_{action.id}`;
@@ -53,49 +105,6 @@ function StrikeOutSwipeServerAction.new(properties: types.ServerActionConstructo
   end;
 
   return action;
-
-end;
-
-function StrikeOutSwipeServerAction.__index:activate(shouldCharge: boolean): ()
-
-  assert(self.contestant.attributes.archetypeMode == "Batter", "Contestant must be in batter mode to use this action.");
-  assert(self.contestant.currentStamina >= self.requiredStamina, "Insufficient stamina.");
-
-  if shouldCharge then
-
-    chargeSwing(self);
-
-  else
-
-    local character = self.contestant.character;
-    local bat = if character then character:FindFirstChild("Bat") else nil;
-    assert(bat and bat:IsA("Accessory"), "The contestant's bat is missing.");
-
-    swingBat(self, bat);
-
-  end;
-
-end;
-
-function StrikeOutSwipeServerAction.__index:breakdown(): ()
-
-  if self.remoteFunction then
-
-    self.remoteFunction:Destroy();
-
-  end;
-
-  if self.remoteEvent then
-
-    self.remoteEvent:Destroy();
-
-  end;
-
-  if self.contestant.player then
-
-    ReplicatedStorage.Shared.Functions.BreakdownAction:InvokeClient(self.contestant.player, self.id);
-
-  end;
 
 end;
 
--- src/ServerStorage/Interfaces/IStrikeOutSwipeServerAction.lua
+++ src/ServerStorage/Interfaces/IStrikeOutSwipeServerAction.lua
@@ -8,6 +8,6 @@ local SharedTypes = require(ServerStorage.Modules.SharedTypes);
 
 type BallType = SharedTypes.BallType
 
-export type IStrikeOutServerAction = IServerAction.IServerAction<{}>;
+export type IStrikeOutSwipeServerAction = IServerAction.IServerAction<{}, (boolean)>;
 
 return {};
