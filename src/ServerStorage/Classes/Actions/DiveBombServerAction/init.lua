bec4041028b1809a8ae5c213527ff2c040f7eb82
Christian Toney <christian.toney@beastslash.com>
2025-03-02 09:28:47 -0500

    update DiveBombServerAction

--- src/ServerStorage/Classes/Actions/DiveBombServerAction/init.lua
+++ src/ServerStorage/Classes/Actions/DiveBombServerAction/init.lua
@@ -5,36 +5,240 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local ServerStorage = game:GetService("ServerStorage");
+local TweenService = game:GetService("TweenService");
 
+local damageFramework = require(ServerStorage.Modules.DamageFramework);
 local DiveBombClientAction = require(ReplicatedStorage.Client.Classes.Actions.DiveBombClientAction);
-local types = require(ServerStorage.Modules.types);
+local IDiveBombServerAction = require(ServerStorage.Interfaces.IDiveBombServerAction);
+local IServerContestant = require(ServerStorage.Interfaces.IServerContestant);
+local IServerRound = require(ServerStorage.Interfaces.IServerRound);
+local ParalysisServerEffect = require(ServerStorage.Classes.Effects.ParalysisServerEffect);
 
 local createInventoryRemoteFunction = require(ServerStorage.Modules.createInventoryRemoteFunction);
-local startAttack = require(script.startAttack);
-local groundDash = require(script.groundDash);
+
+type IServerContestant = IServerContestant.IServerContestant;
+type IServerRound = IServerRound.IServerRound;
+type IDiveBombServerAction = IDiveBombServerAction.IDiveBombServerAction;
 
 local DiveBombServerAction = {
 	id = DiveBombClientAction.id;
 	name = DiveBombClientAction.name;
 	description = DiveBombClientAction.description;
-	__index = {
-		name = DiveBombClientAction.name;
-		id = DiveBombClientAction.id;
-		description = DiveBombClientAction.description;
-	} :: types.DiveBombServerAction;
 };
 
-function DiveBombServerAction.new(properties: types.ServerActionConstructorProperties): types.DiveBombServerAction
+function DiveBombServerAction.new(contestant: IServerContestant, round: IServerRound): IDiveBombServerAction
 
-	local overwrittenProperties = {
-		contestant = properties.contestant;
+	local animationTracks = {};
+
+	local function activate(self: IDiveBombServerAction, coordinates: Vector3, shouldUseTarget: boolean)
+
+		if contestant and contestant.character and contestant.currentHealth > 0 and contestant.currentStamina >= 20 then
+
+			-- Reduce the player's stamina.
+			contestant:setCurrentStamina(math.max(0, contestant.currentStamina - 10), {
+				actionID = self.id;
+			});
+			
+			local primaryPart = contestant.character.PrimaryPart;
+			assert(primaryPart, "Action requires the character to have a primary part.");
+
+			local flightConstraint = primaryPart:FindFirstChild("FlightConstraint");
+			if flightConstraint then
+	
+				local function startAttack()
+
+					local flightConstraint = primaryPart:FindFirstChild("FlightConstraint");
+				
+					if flightConstraint then
+				
+						flightConstraint:Destroy()
+				
+					end
+					
+					primaryPart.CFrame = CFrame.lookAt((primaryPart.CFrame.Position), (coordinates * Vector3.new(1,0,1) + Vector3.new(0,primaryPart.CFrame.Position.Y, 0)));
+				
+					--perhaps some of this could be client side
+					local animData = Vector3.new(0.1,1,1)
+					animationTracks["Right"]:Play(animData.X,animData.Y,animData.Z);
+					animationTracks["Left"]:Play(animData.X,animData.Y,animData.Z);
+					animationTracks["Player"]:Play(animData.X,animData.Y,animData.Z);
+				
+					local part = Instance.new("Part");
+					part.Parent = workspace.Terrain;
+					part.Anchored = true
+					part.CanCollide = false
+					part.Transparency = 1;
+				
+					local rigidConstraint = Instance.new("RigidConstraint");
+					rigidConstraint.Parent = part;
+					rigidConstraint.Attachment0 = Instance.new("Attachment", part);
+				
+					part.CFrame = primaryPart.CFrame
+					rigidConstraint.Attachment1 = primaryPart:FindFirstChild("RootAttachment") :: Attachment;
+				
+					local expectedPos;
+				
+					for i = 1, 5 do
+				
+						expectedPos = part.Position + (( part.CFrame.LookVector * -1 + Vector3.new(0,1/5,0)) * (5-i)) + ((part.CFrame.LookVector + Vector3.new(0,1/5,0)) * (i))
+						local tween = TweenService:Create(part, TweenInfo.new(0.75/5, Enum.EasingStyle.Linear), {Position = expectedPos})
+						tween:Play()
+						task.wait(0.75/5);
+				
+					end
+					animationTracks["Right"]:AdjustSpeed(1.5);
+					animationTracks["Left"]:AdjustSpeed(1.5);
+					animationTracks["Player"]:AdjustSpeed(1.5);
+					
+					local travelTime = 8/15
+					local tween = TweenService:Create(part, TweenInfo.new(travelTime, Enum.EasingStyle.Linear), {
+						Position = coordinates + Vector3.new(0, 0, 0);
+					});
+					tween:Play();
+					task.wait(travelTime*0.8);
+				
+					local data = {
+						size = 15;
+					}
+					damageFramework.explosionEvent(coordinates, data, self, function(contestant)
+					
+						if contestant ~= contestant then
+				
+							local paralysisEffect = ParalysisServerEffect.new({
+								contestant = contestant;
+							});
+				
+							contestant:addEffect(paralysisEffect);
+							task.wait(1);
+				
+							contestant:removeEffect(paralysisEffect);
+				
+						end;
+				
+					end);
+				
+					task.wait(travelTime*0.2);
+					
+					animationTracks["Right"]:AdjustSpeed(1);
+					animationTracks["Left"]:AdjustSpeed(1);
+					animationTracks["Player"]:AdjustSpeed(1);
+					task.wait(0.2)
+					part:Destroy();
+				
+				end;
+
+				startAttack();
+	
+			else
+	
+				local function groundDash()
+
+					local character = contestant.character;
+					assert(character);
+
+					if shouldUseTarget then 
+
+						local objectValue = character:FindFirstChild("Target") :: ObjectValue;
+						if objectValue.Value and objectValue.Value:IsA("Model") and objectValue.Value.PrimaryPart then
+
+							coordinates = objectValue.Value.PrimaryPart.Position;
+
+						end;
+
+					end
+
+					local targetCoords
+					local originalCoords = coordinates;
+					primaryPart.CFrame = CFrame.lookAt((primaryPart.CFrame.Position), (coordinates * Vector3.new(1,0,1) + Vector3.new(0,primaryPart.CFrame.Position.Y, 0)));
+					if (coordinates - primaryPart.Position).Magnitude > 50 then
+						
+						coordinates = (primaryPart.CFrame * CFrame.new(Vector3.new(0,0,-1 * (50)))).Position
+						targetCoords = coordinates
+						originalCoords = targetCoords
+					else
+						targetCoords = coordinates
+						coordinates = CFrame.lookAt(coordinates, primaryPart.Position) * CFrame.new(Vector3.new(0,0,10)).Position
+					end
+
+					--perhaps some of this could be client side
+					local animData = Vector3.new(0.2,1,3)
+					animationTracks["Right"]:Play(animData.X,animData.Y,animData.Z);
+					animationTracks["Left"]:Play(animData.X,animData.Y,animData.Z);
+					animationTracks["Player"]:Play(animData.X,animData.Y,animData.Z);
+					
+					local part = Instance.new("Part");
+					part.Parent = workspace.Terrain;
+					part.Anchored = true
+					part.CanCollide = false
+					part.Transparency = 1;
+					task.wait(0.1)
+					local rigidConstraint = Instance.new("RigidConstraint");
+					rigidConstraint.Parent = part;
+					rigidConstraint.Attachment0 = Instance.new("Attachment", part);
+
+					part.CFrame = primaryPart.CFrame
+					rigidConstraint.Attachment1 = primaryPart:FindFirstChild("RootAttachment") :: Attachment;
+					
+					animationTracks["Right"]:AdjustSpeed(2);
+					animationTracks["Left"]:AdjustSpeed(2);
+					animationTracks["Player"]:AdjustSpeed(2);
+					
+					local travelTime = 8/15
+					local tween = TweenService:Create(part, TweenInfo.new(travelTime, Enum.EasingStyle.Linear), {
+						Position = coordinates + Vector3.new(0, 1, 0);
+					});
+					tween:Play();
+					task.wait(travelTime*0.6);
+					local data = {}
+					damageFramework.explosionEvent(originalCoords, data, self);
+					task.wait(travelTime*0.4);
+
+					
+					animationTracks["Right"]:AdjustSpeed(1);
+					animationTracks["Left"]:AdjustSpeed(1);
+					animationTracks["Player"]:AdjustSpeed(1);
+					task.wait(0.2)
+					part:Destroy();
+
+				end;
+
+				groundDash();
+			
+			end
+	
+		end
+
+	end;
+
+	local function breakdown(self: IDiveBombServerAction)
+
+		if self.remoteFunction then
+
+			self.remoteFunction:Destroy();
+	
+		end
+	
+		if contestant.player then
+	
+			ReplicatedStorage.Shared.Functions.BreakdownAction:InvokeClient(contestant.player, self.id);
+	
+		end;
+
+	end;
+
+	local action: IDiveBombServerAction = {
+		attributes = {};
+		contestantID = contestant.id;
+		description = DiveBombServerAction.description;
+		id = DiveBombServerAction.id;
+		name = DiveBombServerAction.name;
+		activate = activate;
+		breakdown = breakdown;
 	};
 
-	local action = (setmetatable(overwrittenProperties, DiveBombServerAction) :: any) :: types.DiveBombServerAction;
+	if contestant.character then
 
-	if action.contestant.character then
-
-		local function preloadAnims(char: Model)
+		local function preloadAnimations(char: Model)
 
 			local humanoid = char:FindFirstChild("Humanoid") :: Humanoid;
 			local wingProp = (humanoid.Parent :: Instance):FindFirstChild("WingProp") :: Model;
@@ -68,11 +272,11 @@ function DiveBombServerAction.new(properties: types.ServerActionConstructorPrope
 		
 		end
 
-		action.anims = preloadAnims(action.contestant.character);
+		animationTracks = preloadAnimations(contestant.character);
 
 	end;
 
-	local player = action.contestant.player;
+	local player = contestant.player;
 	if player then
 
 		action.remoteFunction = createInventoryRemoteFunction(player, "Action", `{player.UserId}_{action.id}`, function(coordinates, shouldUseTarget)
@@ -89,46 +293,6 @@ function DiveBombServerAction.new(properties: types.ServerActionConstructorPrope
 	end
 
 	return action;
-
-end;
-
-function DiveBombServerAction.__index:activate(coordinates: Vector3, shouldUseTarget: boolean)
-
-	if self.contestant and self.contestant.character and self.contestant.currentHealth > 0 and self.contestant.currentStamina >= 20 then
-
-		-- Reduce the player's stamina.
-		self.contestant:updateStamina(math.max(0, self.contestant.currentStamina - 10), {
-			actionID = self.id;
-		});
-		
-		local flightConstraint = self.contestant.character.PrimaryPart:FindFirstChild("FlightConstraint");
-		if flightConstraint then
-
-			startAttack(self, self.contestant.character.PrimaryPart :: BasePart, self.anims, coordinates);
-
-		else
-
-			groundDash(self, self.contestant.character.PrimaryPart :: BasePart, self.anims, coordinates, shouldUseTarget);
-		
-		end
-
-	end
-
-end;
-
-function DiveBombServerAction.__index:breakdown()
-
-	if self.remoteFunction then
-
-		self.remoteFunction:Destroy();
-
-	end
-
-	if self.contestant.player then
-
-    ReplicatedStorage.Shared.Functions.BreakdownAction:InvokeClient(self.contestant.player, self.id);
-
-  end;
 
 end;
 
--- src/ServerStorage/Interfaces/IServerContestant.lua
+++ src/ServerStorage/Interfaces/IServerContestant.lua
@@ -92,6 +92,8 @@ export type IServerContestantProperties = {
 
   roundID: string;
 
+  character: Model?;
+
   ghostHighlight: Highlight?;
 
   characterRagdollClone: Model?;
